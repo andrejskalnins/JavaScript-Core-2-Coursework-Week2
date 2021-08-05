5337d6cda6f3da7cd30a352951f053118a91e206
andrejskalnins <andrejs.kalnins@gmail.com>
2021-08-05 16:23:18 +0100

    added a version 2 solution to the same problem

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,9 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+  arrayOfPeople.forEach(
+    (person) =>
+      (content.innerHTML += `<h1>${person.name}</h1> <h2>${person.job}</h2>`)
+  );
 }
 
 let people = [
@@ -8,9 +12,27 @@ let people = [
   { name: "Boris", job: "Prime Minister" },
 ];
 
-listOfNames(
-  people.forEach(
-    (person) =>
-      (content.innerHTML += `<h1>${person.name}</h1> <h2>${person.job}</h2>`)
-  )
-);
+listOfNames(people);
+
+// An h2 tag that says "Version 2 Below"
+let subheading = document.createElement("h2");
+subheading.textContent = "Version 2 Below";
+// add in-line style
+subheading.style.color = "orchid";
+content.append(subheading);
+
+// version 2.
+
+function listOfNames(arrayOfPeople) {
+  let content = document.querySelector("#content");
+  arrayOfPeople.forEach((person) => {
+    let nameH1 = document.createElement("h1");
+    let jobH2 = document.createElement("h2");
+    content.appendChild(nameH1);
+    nameH1.textContent = person.name;
+    content.appendChild(jobH2);
+    jobH2.textContent = person.job;
+  });
+}
+
+listOfNames(people);
