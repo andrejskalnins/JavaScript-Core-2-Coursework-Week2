742a080e4e418f619bede7520f7f289ce2ba5cd8
andrejskalnins <andrejs.kalnins@gmail.com>
2021-08-05 16:35:40 +0100

    completed exercise B-shopping-list

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,7 +1,32 @@
 function shoppingList(arrayOfPeople) {
   // Write your code here...
+  let content = document.querySelector("#content");
+  let list = document.createElement("ul");
+  content.appendChild(list);
+  arrayOfPeople.forEach((shoppingItem) => {
+    let listItem = document.createElement("li");
+    listItem.textContent = shoppingItem;
+    list.appendChild(listItem);
+  });
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
 
 shoppingList(shopping);
+
+// Old Attempt
+
+// function shoppingList(arrayOfItems) {
+//   // Write your code here...
+//   let content = document.querySelector("#content");
+//   let list = (content.innerHTML += `<ul></ul>`);
+//   // arrayOfItems.forEach((item) => (content.innerHTML += `<li>${item}</li>`));
+//   let listItems = arrayOfItems.forEach((item) => {
+//     content.innerHTML += `<li>${item}</li>`;
+//   });
+//   list.append(listItems);
+// }
+
+// let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
+
+// shoppingList(shopping);
