a3832c1c246302b61a454567250bdb206bc38234
andrejskalnins <andrejs.kalnins@gmail.com>
2021-08-06 15:20:59 +0100

    completed exercise D-todo-list

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,12 +1,29 @@
 function todoList(todos) {
   // Write your code here...
+  let content = document.querySelector("#content");
+  let toDoList = document.createElement("ul");
+
+  content.appendChild(toDoList);
+
+  todos.forEach((listItem) => {
+    let toDoItems = document.createElement("li");
+    toDoItems.textContent = listItem.todo;
+    toDoList.appendChild(toDoItems);
+    toDoItems.addEventListener("click", function () {
+      if (toDoItems.style.textDecoration === "") {
+        toDoItems.style.textDecoration = "line-through";
+      } else if (toDoItems.style.textDecoration === "line-through") {
+        toDoItems.style.textDecoration = null;
+      }
+    });
+  });
 }
 
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+  { todo: "wash the dishes" },
+  { todo: "walk the dog" },
+  { todo: "learn javascript" },
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
