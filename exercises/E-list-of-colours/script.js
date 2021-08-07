add46c9dcc2340f2c151a4c707cc8eec2573b5e6
andrejskalnins <andrejs.kalnins@gmail.com>
2021-08-07 10:30:59 +0100

    completed E-list-of-colours exercise

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,22 @@
 function listOfColours(colours) {
   // Write your code here...
+  let content = document.querySelector("#content");
+  let selectionList = document.createElement("select");
+  let message = document.createElement("p");
+  content.appendChild(selectionList);
+  content.appendChild(message);
+
+  colours.forEach((colour) => {
+    let colourOption = document.createElement("option");
+    selectionList.appendChild(colourOption);
+    colourOption.innerText = colour;
+    colourOption.value = colour;
+    selectionList.addEventListener("change", function () {
+      let getValue = selectionList.options[selectionList.selectedIndex].value;
+      message.innerText = `You have selected: ${getValue}`;
+      message.style.color = getValue;
+    });
+  });
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
